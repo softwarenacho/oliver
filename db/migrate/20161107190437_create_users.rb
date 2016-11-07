862a245efb84cd4a7c9465fc2a417077ecccde0d
softwarenacho <trsgarp@gmail.com>
2016-11-07 13:15:36 -0600

    Initial configuration

---
+++ db/migrate/20161107190437_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.boolean :admin
+      t.string :email
+      t.string :photo
+      t.string :password
+      t.string :username
+
+      t.timestamps null: false
+    end
+  end
+end
